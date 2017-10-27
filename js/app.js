7d3b5adc73bbe46eac6db1dd782b12d78936403e
EveCantorG <evelyn.cantorg@hotmail.com>
2017-10-27 12:32:48 -0500

    final_proyect_clue

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var testArray = [];
+for(var i = 0; i <= 10; i ++){
+  testArray.push(i);
+}
+console.log(testArray)
+// eliminar el primer elemento del arreglo
+testArray.shift();
+console.log(testArray);
+//Eliminar ultimo elemento del arreglos
+testArray.pop();
+console.log(testArray);
+// Revertir los elementos del arreglo
+testArray.reverse();
+console.log(testArray);
+/*-----------------------------------------------------------------*/
+var testString = "abdcde";
+testString.charAt(2);
+//c
+/*para obtener un caracter
+a partir de su código Ascii
+utilizó el metodo String.fromCharcode*/
+var codigo = 
